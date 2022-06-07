d6e22ff92542aebdb6035273dd463b00dd4fa485
Olivia <ochang@medrhythms.com>
2022-06-07 11:10:48 -0400

    test fix private key

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = new App({
 async function createTwoByTwo(currentTwobytwo) {
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
   });
 
   await doc.loadInfo();
